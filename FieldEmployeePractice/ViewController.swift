188a8c00b42169190f5abdb881ba22288c683b87
Dom Scafati <DomScafati@hotmail.com>
2021-02-15 11:11:41 -0500

    basic practice for class on 1/15/2021

--- FieldEmployeePractice/ViewController.swift
+++ FieldEmployeePractice/ViewController.swift
@@ -7,13 +7,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
 
+    @IBOutlet weak var vcLabel: UILabel!
+    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var vcPickerView: UIPickerView!
+    @IBOutlet weak var vcImageView: UIImageView!
+    @IBAction func vcButton(_ sender: UIButton) {
+        
+        print("go to next page");
+    }
+    let dataSource = ["Ryu", "Ken"]
+    let imageSource = [ #imageLiteral(resourceName: "ryuImage"), #imageLiteral(resourceName: "kenImage") ]
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1;
+    }//number of columns
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        dataSource.count;
+        }//number of rows
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        switch row {
+        case 0:
+            vcImageView.image = #imageLiteral(resourceName: "ryuImage")
+            vcLabel.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            vcLabel.text = "You must defeat Shen Long to stand a chance."
+        case 1:
+            vcImageView.image = #imageLiteral(resourceName: "kenImage")
+            vcLabel.textColor = #colorLiteral(red: 1, green: 0.08404085115, blue: 0, alpha: 1)
+            vcLabel.text = "Lets turn up the heat!"
+        default:
+            vcImageView.image = #imageLiteral(resourceName: "ryuImage")
+            vcLabel.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+
+        }
+        return dataSource[row]
+
+    }
+//
+//    func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+//        return NSAttributedString(string: dataSource[row], attributes: [NSAttributedString.Key.foregroundColor: UIColor.yellow])
+//    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        vcPickerView.dataSource = self;
+        vcPickerView.delegate = self;
     }
-
-
 }
 
